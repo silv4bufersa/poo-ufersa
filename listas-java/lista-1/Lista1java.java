e62e8e5f6fd49250e92fb0360567f08342be7283
Bruno Silva (silv4b) <brunno.linkin@gmail.com>
2025-09-03 18:04:23 -0300

    code: add Lista1Java.java

---
+++ listas-java/lista-1/Lista1java.java
@@ -0,0 +1,16 @@
+package exercicio;
+
+public class Lista1java {
+    /*
+    *
+    * Para cada questão, crie uma classe, como a que está exposta a seguir.
+    * Padrão: questao1(), questao()2 ... questao10(), por exemplo.
+    *
+    * */
+    public static void questao1(){
+        // ...
+    }
+    public static void main(String[] args) {
+        questao1();
+    }
+}
